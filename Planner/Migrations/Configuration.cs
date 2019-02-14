0a44840003f3cff6b88196d6fa8a75ae47bc996c
IvanKukushkin <ivankukushkin@bk.ru>
2019-02-14 07:08:18 +0300

    test msvs asp net web app

---
+++ Planner/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Planner.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PlannerDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PlannerDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Planner/Models/AppointmentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planner.Models
+{
+    public class AppointmentsController : Controller
+    {
+        private PlannerDBContext db = new PlannerDBContext();
+
+        // GET: Appointments
+        public ActionResult Index()
+        {
+            return View(db.Appointments.ToList());
+        }
+
+        // GET: Appointments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // GET: Appointments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,AppointmentDate,Description")] Appointments appointments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointments);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(appointments);
+        }
+
+        // GET: Appointments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // POST: Appointments/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,AppointmentDate,Description")] Appointments appointments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointments).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appointments);
+        }
+
+        // GET: Appointments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointments appointments = db.Appointments.Find(id);
+            if (appointments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointments);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appointments appointments = db.Appointments.Find(id);
+            db.Appointments.Remove(appointments);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
